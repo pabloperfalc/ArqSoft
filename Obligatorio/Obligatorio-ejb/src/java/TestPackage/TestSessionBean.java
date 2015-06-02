f8a8e94bd1c10f5b0d53535627c68d3a48003248
pabloperfalc <pabloperfalc@192.168.1.110>
2015-06-02 19:41:36 -0300

    Segundo commit

---
+++ Obligatorio/Obligatorio-ejb/src/java/TestPackage/TestSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestPackage;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author pabloperfalc
+ */
+@Stateless
+@LocalBean
+public class TestSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
