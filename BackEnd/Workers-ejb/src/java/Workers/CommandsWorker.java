d80013e7b825082450abb4fcf2be81a88b8407cd
Pablo Perez <pabloperfalc@192.168.0.13>
2015-06-19 07:47:59 -0500

    Add queue

---
+++ BackEnd/Workers-ejb/src/java/Workers/CommandsWorker.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Workers;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ *
+ * @author pabloperfalc
+ */
+@MessageDriven(activationConfig = {
+    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/commandsQueue"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+})
+public class CommandsWorker implements MessageListener {
+    
+    public CommandsWorker() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        String a = "sds";
+    }
+    
+}
