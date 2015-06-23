c4c7ff7ce9d8e88b709b1691678bc6fb38b9fa7a
cauito <cauito@air-de-claudio>
2015-06-23 02:09:56 -0300

    Uso la interface (para que acceda con @Remote) que la conoce el ClientSource y el Worker la implementa, trayendo la listade commands

---
+++ Entites/src/Workers/sb/ICommandBeanRemote.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Workers.sb;
+
+import Entities.Command;
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author cauito
+ */
+@Remote
+public interface ICommandBeanRemote {
+    
+    List<Command> getCommandList(int room);
+    
+}
