f4607687a143f15c2ae17ad1c28c9f6cc6cb28cf
cauito <cauito@air-de-claudio>
2015-06-23 02:10:06 -0300

    Uso la interface (para que acceda con @Remote) que la conoce el ClientSource y el Worker la implementa, trayendo la listade commands

--- Workers-ejb/src/java/Workers/sb/CommandBean.java
+++ Workers-ejb/src/java/Workers/sb/CommandBean.java
@@ -5,8 +5,13 @@
  */
 package Workers.sb;
 
+import Entities.Command;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  *
@@ -14,15 +19,23 @@ import javax.ejb.LocalBean;
  */
 @Stateless
 @LocalBean
-public class CommandBean {
+public class CommandBean implements ICommandBeanRemote {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-    
-    
-    /*public List<Command> getCommands(Room myRoom){
-    return 
-    }*/
+
+    @Override
+    public List<Command> getCommandList(int room) {
+        List<Command> lista = new ArrayList<Command>();
+        try {
+            InitialContext ctx = new InitialContext();
+            //PersistenciaRemote per = (PersistenciaRemote) ctx.lookup("java:global/WorkFromHome/Persistencia-ejb/PersistenciaSB");
+            //lista = per.getCommandList();
+        } catch (NamingException ex) {
+           // Logger.getLogger(ProyectoSB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lista;
+    }
     
     
 }
