354cb1e935af3f0ffed74fe3962fca4f55ef1c91
cauito <cauito@172.20.2.82>
2015-06-25 18:07:37 -0300

    Cliente

--- Client/src/com/ArqSoft/Client/Client.java
+++ Client/src/com/ArqSoft/Client/Client.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package client;
+package com.ArqSoft.Client;
 
 import DTOs.User;
 import java.io.BufferedReader;
--- Workers-ejb/src/java/com/ArqSoft/Workers/sb/CommandBean.java
+++ Workers-ejb/src/java/com/ArqSoft/Workers/sb/CommandBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Workers.sb;
+package com.ArqSoft.Workers.sb;
 
 import DTOs.Command;
 import com.ArqSoft.Persistence.PersistenceSb;
